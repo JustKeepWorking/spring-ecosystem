349b53f7830bdcd14c544cbf44d291be786184b6
Hai Nguyen <1533478@hcmut.edu.vn>
2019-05-04 10:18:50 +0700

    add cxf example

---
+++ boot-cxf/src/main/java/com/ecosystem/bootcxf/BootCxfApplication.java
@@ -0,0 +1,13 @@
+package com.ecosystem.bootcxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCxfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootCxfApplication.class, args);
+    }
+
+}
